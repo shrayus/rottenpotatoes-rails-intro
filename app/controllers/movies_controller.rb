8cc5a03b8d627b04ce1857687ebe04dc6d7e5c91
shrayus <shrayusgupta@gmail.com>
2016-02-15 19:47:59 +0000

    testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,6 +18,10 @@ class MoviesController < ApplicationController
     ratings = params[:ratings]
     @ratings = ratings.nil? ? Movie.get_ratings : ratings.keys
     
+    @movies = @movies.where{|m| @ratings.include?(m.rating)}
+    @movies.each do |m|
+      puts m.rating
+    end
     #Sorting by Title/Release
     if(params[:sort].to_s == 'title')
       @movies = @movies.sort_by{|m| m.title }
