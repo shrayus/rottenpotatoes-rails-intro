d88ae3fcdb044a2ced08eeb76083db245bd1c7c6
shrayus <shrayusgupta@gmail.com>
2016-02-15 21:09:40 +0000

    random message

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,10 +18,8 @@ class MoviesController < ApplicationController
     ratings = params[:ratings]
     @ratings = ratings.nil? ? Movie.get_ratings : ratings.keys
     
-    if @movies.length > 0
-      rating_list = "(" + @ratings.to_s[1..-2] + ")" #Because sql arrays have parens instead of brackets T_T 
-      @movies = @movies.where("Rating in " + rating_list) #generate sql query for the website
-    end
+    rating_list = "(" + @ratings.to_s[1..-2] + ")" #Because sql arrays have parens instead of brackets T_T 
+    @movies = @movies.where("Rating in " + rating_list) #generate sql query for the website
     
     #Sorting by Title/Release
     if(params[:sort].to_s == 'title')
